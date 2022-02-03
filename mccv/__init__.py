85396ce45e9f32a6f8eb7a94532404c8e5e7d597
Randall Balestriero <rbalestriero@fb.com>
2022-02-03 08:14:47 -0800

    versioneer

--- mccv/__init__.py
+++ mccv/__init__.py
@@ -1,2 +1,4 @@
 from .SLURM_CV import SLURMConfig
 from .ArgParse_CV import CVArgumentParser
+from . import _version
+__version__ = _version.get_versions()['version']
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import versioneer
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MCCV",
+    version="0.0.1",
+    author="Randall Balestriero",
+    author_email="rbalestriero@fb.com",
+    description="Minimalist argument parser based cross-validation and cluster job submission file generation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    # package_dir={"": "fair_jumpstart"},
+    # packages=setuptools.find_packages(where="fair_jumpstart"),
+    packages=[
+        "mccv",
+    ],
+    python_requires=">=3.6",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
